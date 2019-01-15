d18159fd12e36adc6d6b0721d674e834e5c8da8d
Allyse Fischer <scuba353@gmail.com>
2019-01-15 10:12:18 -0800

    additional coding dojo algos completed 1/14

--- AlgoLectures1.js
+++ AlgoLectures1.js
@@ -172,14 +172,99 @@
 // }
 // console.log(Skyline([-1, 1, 1, 7, 3]))
 
+// //Array: Remove Negatives
+// function RemoveNeg(arr){
+// 	var position= 0
+// 	for(var i=0; i<arr.length; i++){
+// 		if(arr[i]<0){
+// 			continue;
+// 		}
+// 		else{
+// 			arr[position]=arr[i]
+// 			position++
+// 		}
+// 	}
+// 	arr.length=position
+// 	return arr
+// }
+// console.log(RemoveNeg([-1, 2, 4, -10, 12]))
 
+// //Array: Second to Last
+// function SecondToLast(arr){
+// 	if(arr.length<2){
+// 		return null
+// 	}
+// 	else{
+// 		return arr[arr.length-2]
+// 	}
+// }
+// console.log(SecondToLast([42, true, 4, "Kate", 7]))
 
+// //Array: Nth-to-last
+// function NToLast(arr, idx){
+// 	if(arr.length<idx){
+// 		return null
+// 	}
+// 	else{
+// 		return arr[(arr.length-idx)]
+// 	}
+// }
+// console.log(NToLast([42, true, 4, "Kate", 7], 3))
 
+// //Array: Second Largest Value
+// function SecondLargest(arr){
+// 	if(arr.length<2){
+// 		return null;
+// 	}
+// 	for(var i=0; i<arr.length; i++){
+// 		if(arr[i]>arr[i+1]){
+// 			var temp = arr[i];
+// 			arr[i]=arr[i+1];
+// 			arr[i+1]= temp;
+// 		}
+// 	}
+// 	return arr[arr.length-2]
+// }
+// console.log(SecondLargest([42, 1, 4, 3.14, 7]))
 
+// //Array: Nth-Largest
+// function NLargest(arr, idx){
+// 	if(arr.length<idx){
+// 		return null;
+// 	}
+// 	for(var i=0; i<arr.length; i++){
+// 		if(arr[i]>arr[i+1]){
+// 			var temp = arr[i];
+// 			arr[i]=arr[i+1];
+// 			arr[i+1]= temp;
+// 		}
+// 	}
+// 	return arr[arr.length-idx]
+// }
+// console.log(NLargest([42, 1, 4, 3.14, 7], 3))
 
-
-
-
+//Credit Card Validation
+// accept an array of digits of the card (13-19 numbers long)
+// Luhn Formula:
+// 1) Set aside the last digit; do not include it in these caculations until step 5
+// 2) Starting from the back, multiply the digits in odd positions (last, third to last ect) by 2 
+// 3) If any results are larger than 9, subtract 9 from them
+// 4) Add all numbers (not just odds) together
+// 5) Now add the last digit back in- the sum should be a multiple of 10
+function isCreditCardValid(digitArr){
+	//step 1
+	var last= digitArr[digitArr.length-1];
+	digitArr.length= digitArr.length-1;
+	//step 2
+	for(var i=digitArr.length-1; i> 0; i-2){
+		digitArr[i]=digitArr[i]*2
+		if(digitArr[i]>9){
+			digitArr[i]=digitArr[i]-9
+		}
+		console.log(digitArr)
+	}
+}
+isCreditCardValid([5, 2, 2, 8, 2])
 
 
 
